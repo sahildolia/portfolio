dca0aa9b756ccde4fc98fef5c99d1a82de280001
Sahil Dolia <sahil.dolia@sourcemash.com>
2025-09-16 19:29:22 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<h1 className="text-3xl font-bold underline text-blue-600">
+  Hello Tailwind + React 18
+</h1>
+
+    </>
+  )
+}
+
+export default App
